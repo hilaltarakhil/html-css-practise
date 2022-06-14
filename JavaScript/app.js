66f01839c0b9d6ffef9bd3b882e21e73e40177c0
hilaltarakhil <hilal.tarakhil@gmail.com>
2022-06-14 08:41:28 -0400

    arraysJS

---
+++ JavaScript/app.js
@@ -0,0 +1,16 @@
+alert("it's working");
+if (1 === 1){
+  console.log (it's true )
+
+}
+
+if (rating === 2){
+  console.log('super star')
+}
+
+// ARRAY 
+
+let shoppinglist = ['cereal' , 'cheese' , 'ice']
+
+shoppinglist[1] = 'bread';
+
