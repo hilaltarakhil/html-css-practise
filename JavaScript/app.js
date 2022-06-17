ad1c0b35238c94b05e224427eed1b0743d6e65e5
hilaltarakhil <hilal.tarakhil@gmail.com>
2022-06-16 22:42:27 -0400

    JSpush

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,16 +1,38 @@
-alert("it's working");
-if (1 === 1){
-  console.log (it's true )
+// alert("it's working");
+// if (1 === 1){
+//   console.log (it's true )
 
-}
+// }
 
-if (rating === 2){
-  console.log('super star')
-}
+// if (rating === 2){
+//   console.log('super star')
+// }
 
-// ARRAY 
+// // ARRAY
 
-let shoppinglist = ['cereal' , 'cheese' , 'ice']
+// let shoppinglist = ['cereal' , 'cheese' , 'ice']
 
-shoppinglist[1] = 'bread';
+// shoppinglist[1] = 'bread';
 
+// TEST
+
+const tempArray = ["apples", "bananas", "oranges"];
+const anotherArray = ["pears"];
+
+console.log(tempArray.shift());
+
+console.log(tempArray.concat(anotherArray));
+
+console.log(tempArray.reverse());
+
+console.log(tempArray.slice(1));
+
+console.log([...tempArray, ...anotherArray]);
+
+// pop the first element out of the array
+
+//concat both arrays
+
+//reverse one of the arrays
+
+// remove the first element from an array using slice
