11e961eeaa6038328efb46be631559585fc585dc
hilaltarakhil <hilal.tarakhil@gmail.com>
2022-06-25 22:42:24 -0400

    JSadd

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -229,3 +229,14 @@ function fun2() {
   }
   console.log(output);
 }
+
+function myLocalScope() {
+  // Only change code below this line
+
+  console.log("inside myLocalScope", myVar);
+}
+myLocalScope();
+
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+console.log("outside myLocalScope", myVar);
