c4b68782f8410ae53f0973745b025f3424f5e5cb
hilaltarakhil <hilal.tarakhil@gmail.com>
2022-07-07 23:39:35 -0400

    push

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -555,3 +555,18 @@ function isValidPassword(password, username) {
   }
   return true;
 }
+
+// Adding methods to an object!
+const math = {
+  numbers: [1, 2, 3, 4, 5],
+  add: function (x, y) {
+    return x + y;
+  },
+  multiply: function (x, y) {
+    return x * y;
+  }
+}
+
+// To execute multiply:
+math.multiply(5, 9); //45
+
