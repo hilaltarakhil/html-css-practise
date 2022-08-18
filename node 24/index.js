3f450849fbb224d621577911ba8026fa7f0fd6da
hilaltarakhil <hilal.tarakhil@gmail.com>
2022-08-17 23:44:08 -0400

    24js328

--- node 24/index.js
+++ node 24/index.js
@@ -7,3 +7,23 @@ console.log(counterObject.getCounter());
 const newCounterObject = require("./myscript.js");
 
 console.log(newCounterObject.getCounter());
+
+
+// NODE
+
+
+const fs = require("fs");
+
+fs.readdir(process.cwd(), (err) => {
+  if (err) {
+    console.log(err);
+  }
+
+  const allStats = Array(filenames.length).fill(null);
+
+  for (let filename of filenames) {
+    const ondex = filenames.indexof(filename);
+    fs.lstat(filename, (err, stats) => {
+      if (err) {
+        console.log(err);
+      }
